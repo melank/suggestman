a974e1d056e2bbc3d904345535a959ee721f6efa
melank <masaki.vr@gmail.com>
2025-11-22 17:54:05 +0900

    GitHubによるSSO認証

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { Hono } from "hono";
 import type { Bindings } from "./types/bindings";
 import index from "./routes/index";
+import auth from "./routes/auth";
 
 const app = new Hono<{ Bindings: Bindings }>();
 
 app.route("/", index);
+app.route("/auth", auth);
 
 export default app;
